b424efea65291068849e85e863ddcf29c6e7be15
Pyry Kantanen <pyry.kantanen@utu.fi>
2021-02-15 20:45:54 +0200

    Basic functionalities for visualizing areas

---
+++ R/check_api_access.R
@@ -0,0 +1,31 @@
+#' @title Check access to http://geo.stat.fi/geoserver/wfs
+#' @description Check if R has access to resources at http://geo.stat.fi/geoserver/wfs
+#' @export
+#' @author Markus Kainu \email{markus.kainu@@kapsi.fi}
+#' @return a logical.
+#' 
+#' @importFrom httr status_code
+#' @importFrom curl curl_download
+#' 
+#' @examples
+#'  \dontrun{
+#'    check_api_access()
+#'  }
+
+check_api_access <- function(){ 
+  
+  temp <- tempfile()
+  http_url <- "https://kartta.hel.fi/ws/geoserver/avoindata/wfs?request=getCapabilities"
+  
+  suppressWarnings(
+    try(
+      curl::curl_download(http_url, temp, quiet = TRUE), 
+      silent = TRUE)
+  )
+  if (is.na(file.info(temp)$size)) {
+    FALSE
+  }
+  else{
+    TRUE
+  }
+}
---
+++ R/get_areas.R
@@ -0,0 +1,146 @@
+#' Get city areas
+#'
+#' @param city The desired city. Options are Helsinki, Espoo, 
+#' Vantaa, Turku, Tampere, Oulu
+#' @param level The desired administrative level. Options are 1, 2 and 3,
+#' where 1 is the lowest level (= largest areas) and 3 is the highest level 
+#' (= smallest areas)
+#' 
+#' @return sf object
+
+get_areas <- function(city="turku", level=1) {
+  
+  if (city == "turku") {
+    if (level == 1) {
+      url_geo <- "http://dev.turku.fi/datasets/aluejaot/suuralueet-4326.geojson"
+      object <- sf::st_read(dsn = url_geo)
+    }
+    if (level == 2) {
+      url_geo <- "http://dev.turku.fi/datasets/aluejaot/kaupunginosat-4326.geojson"
+      object <- sf::st_read(dsn = url_geo)
+    }
+    if (level == 3) {
+      url_geo <- "http://dev.turku.fi/datasets/aluejaot/pienalueet-4326.geojson"
+      object <- sf::st_read(dsn = url_geo)
+    }
+  }
+  
+  if (city %in% c("helsinki", "vantaa", "espoo")) {
+    
+    wfs_hel <- "https://kartta.hel.fi/ws/geoserver/avoindata/wfs"
+    url <- parse_url(wfs_hel)
+    
+    if (level == 1) {
+      
+      url$query <- list(service = "wfs",
+                        version = "1.1.0",
+                        request = "GetFeature",
+                        typename = "avoindata:Seutukartta_aluejako_suuralue",
+                        srsName = "EPSG:3879",
+                        outputFormat = "GML2")
+    }
+    if (level == 2) {
+      url$query <- list(service = "wfs",
+                        version = "1.1.0",
+                        request = "GetFeature",
+                        typename = "avoindata:Seutukartta_aluejako_tilastoalue",
+                        srsName = "EPSG:3879",
+                        outputFormat = "GML2")
+    }
+    if (level == 3) {
+      url$query <- list(service = "wfs",
+                        version = "1.1.0",
+                        request = "GetFeature",
+                        typename = "avoindata:Seutukartta_aluejako_pienalue",
+                        srsName = "EPSG:3879",
+                        outputFormat = "GML2")
+    }
+      
+      request <- build_url(url)
+      pk_regions <- read_sf(request)
+      
+      if (city == "helsinki") {
+        object <- pk_regions[which(pk_regions$kunta == "091"),]
+      }
+      
+      if (city == "vantaa") {
+        object <- pk_regions[which(pk_regions$kunta == "092"),]
+      }
+      
+      if (city == "espoo") {
+        object <- pk_regions[which(pk_regions$kunta == "049"),]
+      }
+  }
+  
+  if (city == "tampere") {
+    wfs_tam <- "https://geodata.tampere.fi/geoserver/ows"
+    url <- parse_url(wfs_tam)
+    
+    if (level == 1) {
+      
+      url$query <- list(service = "wfs",
+                        version = "1.1.0",
+                        request = "GetFeature",
+                        typename = "hallinnolliset_yksikot:KH_SUURALUE",
+                        srsName = "EPSG:3878",
+                        outputFormat = "GML2")
+    }
+    if (level == 2) {
+      url$query <- list(service = "wfs",
+                        version = "1.1.0",
+                        request = "GetFeature",
+                        typename = "hallinnolliset_yksikot:KH_SUUNNITTELUALUE",
+                        srsName = "EPSG:3878",
+                        outputFormat = "GML2")
+    }
+    if (level == 3) {
+      url$query <- list(service = "wfs",
+                        version = "1.1.0",
+                        request = "GetFeature",
+                        typename = "hallinnolliset_yksikot:KH_TILASTO",
+                        srsName = "EPSG:3878",
+                        outputFormat = "GML2")
+    }
+    
+    request <- build_url(url)
+    tampere_regions <- read_sf(request)
+    object <- tampere_regions
+  }
+  
+  if (city == "oulu") {
+    wfs_oulu <- "https://e-kartta.ouka.fi/TeklaOGCWeb/WFS.ashx"
+    url <- parse_url(wfs_oulu)
+    
+    if (level == 1) {
+      
+      url$query <- list(service = "wfs",
+                        version = "1.1.0",
+                        request = "GetFeature",
+                        typename = "gis:Suuralueet",
+                        srsName = "EPSG:3067",
+                        outputFormat = "GML2")
+    }
+    if (level == 2) {
+      url$query <- list(service = "wfs",
+                        version = "1.1.0",
+                        request = "GetFeature",
+                        typename = "gis:Kaupunginosat",
+                        srsName = "EPSG:3067",
+                        outputFormat = "GML2")
+    }
+    if (level == 3) {
+      url$query <- list(service = "wfs",
+                        version = "1.1.0",
+                        request = "GetFeature",
+                        typename = "gis:Aanestysalueet",
+                        srsName = "EPSG:3067",
+                        outputFormat = "GML2")
+    }
+    
+    request <- build_url(url)
+    oulu_regions <- read_sf(request)
+    object <- oulu_regions
+  }
+  
+    ggplot(object) + geom_sf()
+}
